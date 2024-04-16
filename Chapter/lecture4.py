17a2892bdf835bb22fd301b7b6a7561bedf81ebf
Israt Jahan <140290748+Israt-1234@users.noreply.github.com>
2024-04-16 23:49:46 +0600

    Add files via upload

---
+++ Chapter/lecture4.py
@@ -0,0 +1,16 @@
+name="i am israt jahan from east delta university"
+print(name[0:16])
+
+l_name="hello"
+f_name = "hi"
+print(l_name +" "+ f_name)
+
+print(len(l_name))
+
+print(l_name.upper())
+print(l_name.lower())
+print(l_name.title())
+print(l_name.format())
+print(l_name.encode())
+print(name.replace("a","x"))
+print(name.islower())
