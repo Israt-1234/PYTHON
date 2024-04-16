965866583a4a5cffbf1b0c0b93ee76587fb40ff1
Israt Jahan <140290748+Israt-1234@users.noreply.github.com>
2024-04-16 23:48:20 +0600

    Add files via upload

---
+++ Chapter/lecture11.py
@@ -0,0 +1,19 @@
+i=1
+while(i<=10):
+    print(i)
+    i=i+1
+
+
+for n in range(1,7):
+    for j in range(0,n):
+        print("*",end="")   #new line ignore
+    print()
+
+
+
+r=float(input("enter the input "))
+
+Pi =3.1416
+
+Area=Pi*r*r
+print(f"area of the circle is {Area}")
