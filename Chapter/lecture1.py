87204a34eda166a313cffe312190cc7e46a78971
Israt Jahan <140290748+Israt-1234@users.noreply.github.com>
2024-04-16 23:48:39 +0600

    Add files via upload

---
+++ Chapter/lecture1.py
@@ -0,0 +1,18 @@
+print("Hello world")
+
+print('welcome to my first python program')
+
+#variable       --------this is comment
+
+first_name="israt"
+last_name="jahan"
+print(first_name)
+print(last_name)
+#concat
+print(first_name+" "+last_name)
+
+number=23
+print(number)
+
+
+print("hello israt","hello israt")
