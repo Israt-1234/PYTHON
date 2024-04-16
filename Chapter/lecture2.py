7a00561ad6bdca4cffbdd7259025c88bf90c931e
Israt Jahan <140290748+Israt-1234@users.noreply.github.com>
2024-04-16 23:48:59 +0600

    Add files via upload

---
+++ Chapter/lecture2.py
@@ -0,0 +1,27 @@
+#----datatype------#
+
+#mutable---------> 
+#list,dictionaries,set
+#Immutable-------->
+#string, int, float, Tuples
+
+one="hello is good word"          #string
+two=3456                          #int
+three=33.45                       #float
+four=("nih",233,"ctg")            #Tuple --not changeable
+five=["nihe",567,"dhka"]          #list ---changeable
+six={"name":"hello",              #dictionary
+     "age":45
+     }
+seven={3,4,5,6,7}                #set
+
+print(type(one))
+print(type(two))
+print(type(three))
+print(type(four))
+print(type(five))
+print(type(six))
+print(type(seven))
+
+
+
