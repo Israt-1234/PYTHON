ddf122c824b20c8651824add37af90cff8dbd10d
Israt Jahan <140290748+Israt-1234@users.noreply.github.com>
2024-04-16 23:52:01 +0600

    Add files via upload

---
+++ Chapter/lecture10.py
@@ -0,0 +1,20 @@
+friend=['israt','jahan','hello','hi']
+print(friend[0])
+print(friend[1])
+print(friend[2])
+
+
+for x in friend:
+    print(x)
+
+
+for x in range(1,100):
+    print(x)
+
+sum=0
+num=int(input("enter the first number"))
+num1=int(input("enter the second number"))
+for y in range(num, num1):
+    print(y)
+    sum+=y
+print(sum)
