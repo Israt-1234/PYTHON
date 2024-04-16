714eef3c5f1f5c68aba872063be29fa120b3f943
Israt Jahan <140290748+Israt-1234@users.noreply.github.com>
2024-04-16 23:49:15 +0600

    Add files via upload

---
+++ Chapter/lecture3.py
@@ -0,0 +1,19 @@
+s=4
+c=4
+print(c+s)
+print(c-s)
+print(c*s)
+print(s/c)          #1.0
+
+#int value
+print(s//c)         #1
+
+print(20**2)        #20^2
+
+print(20%3)         #find remainder
+
+
+c=c+1
+c+=1
+
+print(c)
