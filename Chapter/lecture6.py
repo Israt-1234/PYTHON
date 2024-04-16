269d8b29534a19fe542c69223194ae375cdaab13
Israt Jahan <140290748+Israt-1234@users.noreply.github.com>
2024-04-16 23:50:27 +0600

    Add files via upload

---
+++ Chapter/lecture6.py
@@ -0,0 +1,14 @@
+num=input("enter first number ")
+num1=input("enter 2nd number ")
+print(num+num1)
+print(f"sum is {num+num1}")
+print(num+num1)
+#sum=60+4=604
+sum=num+num
+print(sum)
+
+# int value print + operation
+num2=int(input("enter the number "))
+num3=int(input("enter the second num"))
+#sum=60+4=64
+print(num2+num3)
